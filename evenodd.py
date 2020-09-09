47164c6ee72aa5e0fe00fcbad90d97dc237d2f4e
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-09 13:37:50 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,3 @@
+print('Enter a no. to check whether it is even or add')
+a=int(input("Enter value "))
+print("Even") if(a%2==0) else print("Odd")
---
+++ evenodd1.py
@@ -0,0 +1,6 @@
+print('Enter a no. to check whether it is even or add')
+a=int(input("Enter value "))
+if(a%2==0):
+	print("Even") 
+else: 
+	print("Odd")
